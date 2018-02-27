d43dd86866330e8e32dc4dddf69f9af5eaa89f5b
Sanskar Agrawal <sanskaragrawal.coolguy@gmail.com>
2018-02-27 23:35:24 +0530

    Largest path working without numbers

--- cpp_challenge/labyrinth.cpp
+++ cpp_challenge/labyrinth.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void DFS(vector<string> map, vector< vector<int> > &visited, vector<string> &temp_path_map, int &count, int m, int n);
+int DFS(vector<string> map, vector< vector<int> > &visited, vector<string> &path_map, int m, int n);
 int isvalid(vector<string> map, int i, int j);
 
 int main()
@@ -34,6 +34,7 @@ int main()
 		visited.push_back(temp);
 	}
 
+	vector< vector<int> > visited_final = visited;
 	int max_count = 0;
 	for(int i = 0; i < map.size(); i++)
 	{
@@ -41,17 +42,19 @@ int main()
 		{
 			if(visited[i][j] != 0)
 				continue;
+			vector< vector<int> > visited_temp = visited;
 			int current_count = 0;
 			vector<string> temp_path_map = map;
-			DFS(map, visited, temp_path_map, current_count, i, j);
+			current_count = DFS(map, visited_temp, temp_path_map, i, j);
 			if(current_count > max_count)
 			{
 				final_path_map = temp_path_map;
 				max_count = current_count;
+				visited_final = visited_temp;
 			}
 		}
 	}
-	
+	cout<<"\nLength of path : "<<max_count;
 	cout<<"\nFinal traversal path is \n";
 	for(int i=0; i < final_path_map.size(); i++)
 		cout<<final_path_map[i]<<endl;
@@ -68,19 +71,64 @@ int isvalid(vector<string> map, int i, int j)
 	return 1;
 }
 
-void DFS(vector<string> map, vector< vector<int> > &visited, vector<string> &temp_path_map, int &count, int m, int n)
+int DFS(vector<string> map, vector< vector<int> > &visited, vector<string> &path_map, int m, int n)
 {
 	// cout<<count<<endl;
-	if(isvalid(map, m, n) == 0) return;
-	temp_path_map[m][n] = '*';
-	count++;
+	if(isvalid(map, m, n) == 0) return 0;
+	path_map[m][n] = '*';
 	visited[m][n] = 1;
+
+	int max_count = 0;
+	vector< vector<int> > temp_visited_1, temp_visited_2, temp_visited_3, temp_visited_4;
+	vector<string> temp_path_map_1,temp_path_map_2,temp_path_map_3,temp_path_map_4;
+
+	temp_visited_1=temp_visited_2=temp_visited_3=temp_visited_4=visited;
+	temp_path_map_1=temp_path_map_2=temp_path_map_3=temp_path_map_4=path_map;
+
 	if(isvalid(map,m-1,n))
-		if(visited[m-1][n] == 0) DFS(map, visited, temp_path_map, count, m-1, n);
+		if(visited[m-1][n] == 0)
+		{
+			int count = DFS(map, temp_visited_1, temp_path_map_1, m-1, n);
+			if(count > max_count)
+			{
+				path_map = temp_path_map_1;
+				visited = temp_visited_1;
+				max_count = count;
+			}
+		}
 	if(isvalid(map,m,n-1))
-		if(visited[m][n-1] == 0) DFS(map, visited, temp_path_map, count, m, n-1);
+		if(visited[m][n-1] == 0)
+		{
+			int count = DFS(map, temp_visited_2, temp_path_map_2, m, n-1);
+			if(count > max_count)
+			{
+				path_map = temp_path_map_2;
+				visited = temp_visited_2;
+				max_count = count;
+			}
+		}
 	if(isvalid(map,m+1,n))
-		if(visited[m+1][n] == 0) DFS(map, visited, temp_path_map, count, m+1, n);
+		if(visited[m+1][n] == 0)
+		{
+			int count = DFS(map, temp_visited_3, temp_path_map_3, m+1, n);
+			if(count > max_count)
+			{
+				path_map = temp_path_map_3;
+				visited = temp_visited_3;
+				max_count = count;
+			}
+		}
 	if(isvalid(map,m,n+1))
-		if(visited[m][n+1] == 0) DFS(map, visited, temp_path_map, count, m, n+1);
+		if(visited[m][n+1] == 0)
+		{
+			int count = DFS(map, temp_visited_4, temp_path_map_4, m, n+1);
+			if(count > max_count)
+			{
+				path_map = temp_path_map_4;
+				visited = temp_visited_4;
+				max_count = count;
+			}
+		}
+
+	return 1 + max_count;
 }
